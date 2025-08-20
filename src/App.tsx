57b2dc90a78a7dbf5f0615cc13789c173d193db0
YuliaProkipchuk <yulayulayula2003@gmail.com>
2025-08-20 18:17:07 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
-// import { getAll, get5First, getRed } from './api/goods';
-// or
-// import * as goodsAPI from './api/goods';
+import { getAll, get5First, getRedGoods } from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+enum Mode {
+  All,
+  First5,
+  Red,
+  None,
+}
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
+  const [mode, setMode] = useState<Mode>(Mode.None);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  useEffect(() => {
+    switch (mode) {
+      case Mode.All: {
+        getAll().then((data: Good[]) => setGoods(data));
+        break;
+      }
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+      case Mode.First5: {
+        get5First().then((data: Good[]) => setGoods(data));
+        break;
+      }
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+      case Mode.Red: {
+        getRedGoods().then((data: Good[]) => setGoods(data));
+        break;
+      }
 
-    <GoodsList goods={[]} />
-  </div>
-);
+      default:
+        setGoods([]);
+    }
+  }, [mode]);
+
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button
+        type="button"
+        data-cy="all-button"
+        onClick={() => setMode(Mode.All)}
+      >
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={() => setMode(Mode.First5)}
+      >
+        Load 5 first goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="red-button"
+        onClick={() => setMode(Mode.Red)}
+      >
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
